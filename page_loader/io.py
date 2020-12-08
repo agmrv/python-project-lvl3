9442825f2c78cf6aa1a1dce50f71d9e9e1deee74
agmrv <airatgum@gmail.com>
2020-12-08 10:59:49 +0500

    draft

--- page_loader/io.py
+++ page_loader/io.py
@@ -8,24 +8,18 @@ from bs4 import BeautifulSoup
 
 
 
-def normilize_str(string):
-    parts = re.split(r'[^\da-zA-Z]', string)
-    normilized_str = '-'.join(filter(None, parts))
-    return normilized_str
-
-
-def generate_filename(netloc, some_path):
-
-    raw_filename = f'{netloc}-{some_path}'
-    filename = normilize_str(raw_filename)
-
-    return filename
-
 
 def download(url, output_path):
+    
+    if not path.exists(output_path):
+        raise FileExistsError(f"directory '{output_path}' not found")
+    elif not path.isdir(output_path):
+        raise ValueError(f"'{output_path}' is not a directory")
+
+
     parsed_url = urlparse(url)
     netloc = parsed_url.netloc
-    
+
     filename = generate_filename(netloc, parsed_url.path)
     filepath = path.join(output_path, f'{filename}.html')
     dirpath = path.join(output_path, f'{filename}_files')
@@ -42,14 +36,13 @@ def download(url, output_path):
     for img in imgs:
         print(img.get('src'))
 
+    if not path.exists(dirpath):
+        mkdir(dirpath)
 
     with open(filepath, 'w') as file_object:
         file_object.write(response.text)
 
-    try:
-        mkdir(dirpath)
-    except OSError:
-        pass
+
 
 
     # root, ext = path.splitext(url)
@@ -59,3 +52,17 @@ def download(url, output_path):
 
 
     return filepath
+
+
+def generate_filename(netloc, some_path):
+
+    raw_filename = f'{netloc}-{some_path}'
+    filename = normilize_str(raw_filename)
+
+    return filename
+
+
+def normilize_str(string):
+    parts = re.split(r'[^\da-zA-Z]', string)
+    normilized_str = '-'.join(filter(None, parts))
+    return normilized_str
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -5,8 +5,14 @@ from page_loader import download
 
 def main():
     args = parse_args()
-    download(args.url, args.output)
-
+    try:
+        file_path = download(args.url, args.output)
+    except FileExistsError as exists_error:
+        print(exists_error)
+    except ValueError as value_error:
+        print(value_error)
+    else:
+        print(file_path)
 
 if __name__ == '__main__':
     main()
